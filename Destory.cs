233b077a6ff84bafc8c3a77fdc2f643ca5e04eac
Baroma <horhenglee123@gmail.com>
2025-01-18 10:05:18 +0700

    Add files via upload

---
+++ Destory.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Destory : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // Destorying the gameObject upon starting the game.
+        Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
